d677f39aeb6fab79aa0db87049cc944ef4df3f5c
Matt Reisdorf <mattreisdorf@Matts-MacBook-Pro.local>
2021-05-11 14:44:58 -0400

    user routes api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const lessonRoutes = require('./lessonRoutes');
 const notesRoutes = require('./notesRoutes');
+const userRoutes = require('./userRoutes')
 
 router.use('/lessons', lessonRoutes);
 router.use('/notes', notesRoutes);
+router.use('/users', userRoutes);
 
 
 module.exports = router;
